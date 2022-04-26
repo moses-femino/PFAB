3f5e16f35de9ba5573b3c28cee86ceb81bb80c59
moses-femino <momofem2003@gmail.com>
2022-04-25 21:23:04 -0600

    section-06
    work for section 6

--- section-06/factorial.py
+++ section-06/factorial.py
@@ -1,1 +1,11 @@
-#no idea
+number = int(input("Enter a number"))
+
+
+def factorial(number):
+    product = 1
+    while number > 0:
+        product *= number
+        number -= 1
+    return product
+
+print("The factorial of " + str(number) + " is " + str(factorial(number)))
--- section-06/sum_of_numbers.py
+++ section-06/sum_of_numbers.py
@@ -1,3 +1,13 @@
-number = int(input("Please enter a positive number"))
+user_number = int(input("Please enter a positive number"))
+sum = 0
+number = user_number
 
-#i have no idea
+
+while user_number > 0:
+    sum += user_number
+    user_number -= 1 
+
+print(sum)
+print(number)
+
+
