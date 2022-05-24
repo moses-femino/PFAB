163145f274df38d92d5ba510635bc54b3317ee0a
moses-femino <momofem2003@gmail.com>
2022-05-24 11:17:46 -0700

    section-09

---
+++ section-09/intro_to_dict_exercise.py
@@ -0,0 +1,4 @@
+my_dictionary = {"marcus smart": 1, "jaylen brown": 2, "jayson tatum": 3, "robert williams": 4, "al horford": 5}
+print(my_dictionary["jayson tatum"])
+print("grant williams" in my_dictionary)
+print("derrick white" not in my_dictionary)
