d1c37dee80fa746969e04653a7faa7e198d29bc0
moses-femino <momofem2003@gmail.com>
2022-05-24 18:38:08 -0700

    section-09

--- section-09/dict_methods_2_exercise.py
+++ section-09/dict_methods_2_exercise.py
@@ -1,9 +1,13 @@
-for key, value in {}.fromkeys("bcdfghjklmnpqrstvwxyz", "consonant").items():
+from multiprocessing.sharedctypes import Value
+
+
+consonants = {}.fromkeys("bcdfghjklmnpqrstvwxyz", "consonant")
+for key, value in consonants.items():
     print(key, value)
 
 fast_food_items = {"McDonald's": "Big Mac", "Burger King": "Whopper", "Chick-fil-A": "Original Chicken Sandwich"}
-print(fast_food_items.pop("McDonald's"))
+mcdonald = fast_food_items.pop("McDonald's")
+print(mcdonald)
 
 fast_food_items.popitem()
 print(fast_food_items)
-#weird things going on here... c comes first and it won't pop cfa
