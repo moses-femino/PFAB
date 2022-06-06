ea1e652db9cb9b99ea2beb080941ffabd70faeeb
moses-femino <momofem2003@gmail.com>
2022-06-05 20:10:56 -0700

    project-01

--- project-01/project_1.py
+++ project-01/project_1.py
@@ -1,9 +1,12 @@
 from random import randint
 shopping_cart = input("Please enter the SKU of the item you would like to purchase")
+objects = input("What quantity of this item do you want to purchase")
+
 coupon = randint(10,30)
-sales_tax = randint(6,12)
+sales_tax = randint(6,13)
 
 print("Congratulations! You have recieived a coupon that will save you " + str(coupon) + "%!")
+#print("You have purchased ")
 #print("Your subtotal after the discount is " )
 #print("Your subtotal before taxes is ")
 #print("Your total after all discounts and taxes is ")
