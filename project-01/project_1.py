8c37959b2a295d28e145ee95a80fe651d6ecad14
moses-femino <momofem2003@gmail.com>
2022-06-28 19:00:28 -0700

    project-01

--- project-01/project_1.py
+++ project-01/project_1.py
@@ -1,33 +1,40 @@
 from random import randint
 
-new_product = {}
+coupon = randint(10,30)
+sales_tax = randint(6,13)
+
 product = ""
-items_list = []
+item_list = []
 while product != "Done":
     product = input("Please enter an item(SKU, description, unit price, quantinty) or Done ")
     item = product.split(",")
-    print(item)
+    #print(product)
     if product != "Done":
+        #print(item)
+        new_product = {}
         new_product["sku"] = item[0]
         new_product["description"] = item[1]
         new_product["unit_price"] = item[2]
         new_product["quantity"] = item[3]
-        items_list.append(new_product)
-    
-print("Shopping complete")
-print(items_list)
+        item_list.append(new_product)
+
+print(item_list)
+
+subtotal = 0 
+for item in item_list:
+    print(item)
+    #multiply unit_price by quantity for each item and add
+    price = float(item["unit_price"]) * int(item["quantity"])
+    subtotal = subtotal + price
+print("Your subtotal is $ " + str(subtotal))
 
 
-
-""" objects = input("What quantity of this item do you want to purchase")
-
-coupon = randint(10,30)
-sales_tax = randint(6,13)
-
+""" print("Shopping complete")
+print(item_list)
 print("Congratulations! You have recieived a coupon that will save you " + str(coupon) + "%!")
-#print("You have purchased ")
+print("The sales tax on your order is " + str(sales_tax) + "%.")
 #print("Your subtotal after the discount is " )
 #print("Your subtotal before taxes is ")
 #print("Your total after all discounts and taxes is ")
-#print("Thank you for shopping with us! Have a great day :)")
+print("Thank you for shopping with us! Have a great day :)")
  """
